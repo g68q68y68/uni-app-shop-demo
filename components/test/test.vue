8ef6f5b4427915c7616ddd6a2a2744e6f81726b6
ganqingyao <1421932626@qq.com>
2024-12-27 21:20:11 +0800

    初始项目

---
+++ components/test/test.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		test
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"test",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
